937e864e7a2b88f4e027cd267a54f91dac5c18dc
chaos yu <chaos.you@gmail.com>
2017-12-05 00:04:54 +0800

    complete lua callback support, update the example page

--- defos/src/defos_win.c
+++ defos/src/defos_win.c
@@ -11,9 +11,6 @@
 // keep track of window placement when going to/from fullscreen or maximized
 WINDOWPLACEMENT placement = { sizeof(placement) };
 
-/**
- * window message hook
- * **/
 // used to check if mouse tracking enabled
 bool is_mouse_tracking=false;
 
@@ -31,123 +28,20 @@ struct LuaCallbackInfo
 
 // callback of mouse state
 
-LuaCallbackInfo* mouseStateCb;
+LuaCallbackInfo mouseStateCb;
 
-// TODO: make a meaningful name
-// register can be a general one
-void register_callback(lua_State* L, int index, LuaCallbackInfo* cbk)
-{
-	
-	luaL_checktype(L, index, LUA_TFUNCTION);
-    lua_pushvalue(L, index);
-	int cb = dmScript::Ref(L, LUA_REGISTRYINDEX);
+/*function declarations*/
+bool set_window_style(LONG_PTR style);
+LONG_PTR get_window_style();
+bool enable_mouse_tracking();
+int __stdcall custom_wndproc(HWND hwnd, UINT umsg, WPARAM wp, LPARAM lp);
+void register_callback(lua_State* L, int index, LuaCallbackInfo* cbk);
+void unregister_callback(LuaCallbackInfo* cbk);
+void invoke_mouse_state(int state);
 
-	if(cbk->m_Callback != LUA_NOREF)
-    {
-        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Callback);
-        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Self);
-    }
-
-    cbk->m_L = dmScript::GetMainThread(L);
-
-
-    cbk->m_Callback = cb;
-
-    dmScript::GetInstance(L);
-    cbk->m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
-
-}
-
-void unregister_callback(LuaCallbackInfo* cbk)
-{
-    if(cbk->m_Callback != LUA_NOREF)
-    {
-        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Callback);
-        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Self);
-        cbk->m_Callback = LUA_NOREF;
-    }
-}
-
-// 0-leave, 1-hover
-void invoke_mouse_state(int state)
-{
-	if(mouseStateCb->m_Callback == LUA_NOREF)
-    {
-        return;
-    }
-
-    lua_State* L = mouseStateCb->m_L;
-    int top = lua_gettop(L);
-
-    lua_rawgeti(L, LUA_REGISTRYINDEX, mouseStateCb->m_Callback);
-
-    // Setup self (the script instance)
-    lua_rawgeti(L, LUA_REGISTRYINDEX, mouseStateCb->m_Self);
-    lua_pushvalue(L, -1);
-
-    dmScript::SetInstance(L);
-
-    lua_pushnumber(L, state);
-
-    int ret = lua_pcall(L, 2, 0, 0);
-
-    if(ret != 0) {
-        dmLogError("Error running callback: %s", lua_tostring(L, -1));
-        lua_pop(L, 1);
-    }
-    assert(top == lua_gettop(L));
-}
-
-void defos_set_window_title(const char* title_lua);
-
-// enable the mouse tracking event, so that wndproc will recieve the mouse leave/hover event
-bool enable_mouse_tracking()
-{
-	HWND window = dmGraphics::GetNativeWindowsHWND();
-
-	TRACKMOUSEEVENT tme;
-	tme.cbSize = sizeof(TRACKMOUSEEVENT);
-	tme.dwFlags = TME_HOVER | TME_LEAVE;
-	tme.hwndTrack = window;
-	tme.dwHoverTime = 1;
-
-	return TrackMouseEvent(&tme);
-}
-
-// replaced wndproc to cutomize message processing
-int __stdcall custom_wndproc(HWND hwnd, UINT umsg, WPARAM wp, LPARAM lp)
-{
-	// do nothing with msg
-	
-	// NOTE: we do not handle any event here, so they will be processed by the default wndproc callback
-
-	switch (umsg)
-	{
-		case WM_MOUSEMOVE:
-			if(!is_mouse_tracking){
-				is_mouse_tracking = enable_mouse_tracking();
-			}
-			break;
-		case WM_MOUSELEAVE:
-			is_mouse_tracking = false; // each time leave and hover reached, the mouse tracking is disabled, we need to track it again
-			
-			// TODO: here we should invoke a callback from Lua side to make it easy to handle, how?
-			defos_set_window_title("mouse leave");
-			invoke_mouse_state(0);
-			break;
-		case WM_MOUSEHOVER:
-			is_mouse_tracking = false; 
-			defos_set_window_title("mouse enter");
-			invoke_mouse_state(1);
-			break;
-	}
-
-	if (originalProc != NULL)
-		
-		return CallWindowProc(originalProc, hwnd, umsg, wp, lp);
-	else
-		return 0;
-}
+/******************
+ * expose functions
+ ******************/
 
 // subclass the window
 bool defos_enable_subclass_window()
@@ -156,38 +50,29 @@ bool defos_enable_subclass_window()
 
 	originalProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)&custom_wndproc); // keep original proc
 
-	defos_set_window_title("hooked");
-
 	if (originalProc == NULL)
 	{
 		DWORD error = GetLastError();
 
 		// error handling? how?
-		printf("error:%d\n", error);
+		dmLogError("error while subclass current window: %d\n", error);
 
 		return false;
 	}
 	return true;
 }
 
+// disable subclass windows
 void defos_disable_subclass_window()
 {
 	HWND window = dmGraphics::GetNativeWindowsHWND();
 	SetWindowLongPtr(window, -4, (LONG)originalProc);
 }
 
+// watch the mouse state changing
 void defos_watch_mouse_state(lua_State* L)
 {	
-	printf("1");
-	register_callback(L, 1, mouseStateCb);
-}
-
-bool set_window_style(LONG_PTR style) {
-	return SetWindowLongPtrA(dmGraphics::GetNativeWindowsHWND(), GWL_STYLE, style) != 0;
-}
-
-LONG_PTR get_window_style() {
-	return GetWindowLongPtrA(dmGraphics::GetNativeWindowsHWND(), GWL_STYLE);
+	register_callback(L, 1, &mouseStateCb);
 }
 
 bool defos_is_fullscreen() {
@@ -304,6 +189,125 @@ void defos_set_window_size(int x, int y, int w, int h) {
 
 void defos_set_window_title(const char* title_lua) {
 	SetWindowTextW(dmGraphics::GetNativeWindowsHWND(), CA2W(title_lua));
+}
+
+/********************
+ * internal functions
+ ********************/
+
+bool set_window_style(LONG_PTR style) {
+	return SetWindowLongPtrA(dmGraphics::GetNativeWindowsHWND(), GWL_STYLE, style) != 0;
+}
+
+LONG_PTR get_window_style() {
+	return GetWindowLongPtrA(dmGraphics::GetNativeWindowsHWND(), GWL_STYLE);
+}
+
+// enable the mouse tracking event, so that wndproc will recieve the mouse leave/hover event
+bool enable_mouse_tracking()
+{
+	HWND window = dmGraphics::GetNativeWindowsHWND();
+
+	TRACKMOUSEEVENT tme;
+	tme.cbSize = sizeof(TRACKMOUSEEVENT);
+	tme.dwFlags = TME_HOVER | TME_LEAVE;
+	tme.hwndTrack = window;
+	tme.dwHoverTime = 1;
+
+	return TrackMouseEvent(&tme);
+}
+
+// replaced wndproc to cutomize message processing
+int __stdcall custom_wndproc(HWND hwnd, UINT umsg, WPARAM wp, LPARAM lp)
+{
+	// NOTE: we do not handle any event here, so they will be processed by the default wndproc callback
+
+	switch (umsg)
+	{
+		case WM_MOUSEMOVE:
+			if(!is_mouse_tracking){
+				is_mouse_tracking = enable_mouse_tracking();
+			}
+			break;
+		case WM_MOUSELEAVE:
+			is_mouse_tracking = false; // each time leave and hover reached, the mouse tracking is disabled, we need to track it again
+			
+			invoke_mouse_state(0);
+			break;
+		case WM_MOUSEHOVER:
+			is_mouse_tracking = false; 
+			invoke_mouse_state(1);
+			break;
+	}
+
+	if (originalProc != NULL)
+		return CallWindowProc(originalProc, hwnd, umsg, wp, lp);
+	else
+		return 0;
+}
+
+// TODO: make a meaningful name
+// register can be a general one
+void register_callback(lua_State* L, int index, LuaCallbackInfo* cbk)
+{
+	luaL_checktype(L, index, LUA_TFUNCTION);
+    lua_pushvalue(L, index);
+	int cb = dmScript::Ref(L, LUA_REGISTRYINDEX);
+
+	if(cbk->m_Callback != LUA_NOREF)
+    {
+        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Callback);
+        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Self);
+    }
+
+    cbk->m_L = dmScript::GetMainThread(L);
+    cbk->m_Callback = cb;
+
+    dmScript::GetInstance(L);
+
+    cbk->m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
+}
+
+void unregister_callback(LuaCallbackInfo* cbk)
+{
+    if(cbk->m_Callback != LUA_NOREF)
+    {
+        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Callback);
+        dmScript::Unref(cbk->m_L, LUA_REGISTRYINDEX, cbk->m_Self);
+        cbk->m_Callback = LUA_NOREF;
+    }
+}
+
+// invoke the mouse state change callback, 0-leave, 1-hover
+void invoke_mouse_state(int state)
+{
+	LuaCallbackInfo* mscb = &mouseStateCb;
+
+	if(mscb->m_Callback == LUA_NOREF)
+    {
+        return;
+    }
+
+    lua_State* L = mscb->m_L;
+    int top = lua_gettop(L);
+
+    lua_rawgeti(L, LUA_REGISTRYINDEX, mscb->m_Callback);
+
+    // Setup self (the script instance)
+    lua_rawgeti(L, LUA_REGISTRYINDEX, mscb->m_Self);
+    lua_pushvalue(L, -1);
+
+    dmScript::SetInstance(L);
+
+    lua_pushnumber(L, state);
+
+    int ret = lua_pcall(L, 2, 0, 0);
+
+    if(ret != 0) {
+        dmLogError("Error running callback: %s", lua_tostring(L, -1));
+        lua_pop(L, 1);
+    }
+    assert(top == lua_gettop(L));
 }
 
 #endif
